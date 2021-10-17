9952ee84e389cb6c5a28ee98193ea90c185be8a3
dell11 <sasikumar.raju@ideas2it.com>
2021-10-17 22:58:50 +0530

    First Commit

---
+++ EmployeeManagement.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Ideas2It Technologies, Inc. All Rights Reserved.
+ */
+import com.ideas2it.project.view.EmployeeView;
+
+/**
+ * Employee Management System main method class
+ * 
+ * @version	1.0
+ * @date	01 Oct 2021
+ * @author	Sasikumar Raju
+ */
+public class EmployeeManagement {
+    
+    public static void main(String[] args) {        
+       	EmployeeView employeeView  = new EmployeeView();	
+        employeeView.getMainMenu(); 
+    }
+ 
+}
+
+
+
